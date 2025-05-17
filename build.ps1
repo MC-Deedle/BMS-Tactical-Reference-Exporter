931faf78e7ec5d9e613b87c11ea717d06affbae1
MC-Deedle <yanikins@gmail.com>
2025-05-17 14:36:22 +1000

    Some fixes

--- build.ps1
+++ build.ps1
@@ -1,1 +1,6 @@
- pyinstaller tacref-exporter.py -F   
+del -r dist
+pip install pyinstaller
+pip install -r requirements.txt
+
+pyinstaller --onefile -w .\tacref-exporter.py
+# Compress-Archive -Update .\"tac-ref exporter 1.0"\ "tac-ref exporter 1.0.zip"
